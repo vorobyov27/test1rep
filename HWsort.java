157ba3bbdfc80cd4ce84ff23fe13f49e9506957a
Alexander Vorobyov <vorobyov27@gmail.com>
2018-11-21 23:44:19 +0300

    add modified cycle with WHILE

--- HWsort.java
+++ HWsort.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 public class HWsort {
     public static void main (String[] args)
     {
-        int [] array = {9,0,7,3,1,5,4,8};
-        for (int i = 0; i<array.length; i++)
+       int [] array = {4,2,55,11,22,2,6,8,2,3,0};
+  /*      for (int i = 0; i<array.length; i++)
         {
             for (int j = 0; j < array.length -1;j++)
             {   int a = 0;
@@ -16,6 +16,23 @@ public class HWsort {
                         array[j+1] = a;
                 }
             }
+        }
+        System.out.println(Arrays.toString(array)); */
+        int i =0;
+        while (i <array.length)
+        {
+            int j = 0;
+            while (j < array.length -1)
+            {   int a = 0;
+                if (array[j]>array[j+1])
+                {
+                    a = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = a;
+                }
+                j++;
+            }
+            i++;
         }
         System.out.println(Arrays.toString(array));
 
